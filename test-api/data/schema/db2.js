30163925ee039f25d13419fff288a53e6807f0cd
Jeffrey Tran <jeffrey.tran@dps.texas.gov>
2017-10-03 07:32:42 -0500

    Added creates and drops for DB2 database schema.  WIP: needs unique and FK constraints.

--- test-api/data/schema/db2.js
+++ test-api/data/schema/db2.js
@@ -1,5 +1,5 @@
-const dropAccountsTable = `DROP TABLE accounts`;
-const createAccountsTable = `CREATE TABLE accounts (
+module.exports.dropAccountsTable = `DROP TABLE accounts`;
+module.exports.createAccountsTable = `CREATE TABLE accounts (
   id INTEGER,
   email_address VARCHAR(150),
   first_name VARCHAR(255),
@@ -8,7 +8,57 @@ const createAccountsTable = `CREATE TABLE accounts (
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 )`;
 
-module.exports = {
-  dropAccountsTable,
-  createAccountsTable
-}
+// ALTER TABLE "accounts" ADD (
+//   CONSTRAINT accounts_pk PRIMARY KEY ("id"))
+
+module.exports.dropCommentsTable = `DROP TABLE comments`;
+module.exports.createCommentsTable = `CREATE TABLE comments (
+  id INTEGER ,
+  body VARCHAR(4000) NOT NULL,
+  post_id INTEGER NOT NULL,
+  author_id INTEGER NOT NULL,
+  archived INTEGER DEFAULT 0,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)`;
+
+// ALTER TABLE "comments" ADD (
+//   CONSTRAINT comments_pk PRIMARY KEY ("id"))
+
+module.exports.dropPostsTable = `DROP TABLE posts`;
+module.exports.createPostsTable = `CREATE TABLE posts (
+  id INTEGER ,
+  body VARCHAR(4000) NOT NULL,
+  author_id INTEGER NOT NULL,
+  archived INTEGER DEFAULT 0,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)`;
+
+// ALTER TABLE "posts" ADD (
+  // CONSTRAINT posts_pk PRIMARY KEY ("id"))
+
+
+module.exports.dropRelationshipsTable = `DROP TABLE relationships`;
+module.exports.createRelationshipsTable = `CREATE TABLE relationships (
+  follower_id INTEGER NOT NULL,
+  followee_id INTEGER NOT NULL,
+  closeness VARCHAR(255),
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  UNIQUE (follower_id, followee_id)
+)`;
+
+module.exports.dropLikesTable = `DROP TABLE likes`;
+module.exports.createLikesTable = `CREATE TABLE likes (
+  account_id INTEGER NOT NULL,
+  comment_id INTEGER NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  UNIQUE (account_id, comment_id)
+)`;
+
+module.exports.dropSponsorsTable = `DROP TABLE sponsors`;
+module.exports.createSponsorsTable = `CREATE TABLE sponsors (
+  generation INTEGER NOT NULL,
+  first_name VARCHAR(255),
+  last_name VARCHAR(255),
+  num_legs INTEGER DEFAULT 2,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)`;
